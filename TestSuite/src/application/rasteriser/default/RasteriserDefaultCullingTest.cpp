4acc10eefb73bc12350c631c76d96c262442e2d4
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-08 10:06:20 +0100

    RasteriserDefaultCullingTest initialise function defined

--- TestSuite/src/application/rasteriser/default/RasteriserDefaultCullingTest.cpp
+++ TestSuite/src/application/rasteriser/default/RasteriserDefaultCullingTest.cpp
@@ -1,6 +1,7 @@
 #include "RasteriserDefaultCullingTest.h"
 
 RasteriserDefaultCullingTest::RasteriserDefaultCullingTest()
+	: device{ nullptr }, context{ nullptr }, description()
 {
 }
 
@@ -10,28 +11,10 @@ RasteriserDefaultCullingTest::~RasteriserDefaultCullingTest()
 
 std::string RasteriserDefaultCullingTest::test()
 {
-	ID3D11Device* device;
-	ID3D11DeviceContext* context;
-	CD3D11_RASTERIZER_DESC description;
-
-	D3D_FEATURE_LEVEL levels[] = {
-	D3D_FEATURE_LEVEL_11_0
-	};
-
-	D3D_FEATURE_LEVEL supported;
-
-	D3D11CreateDevice(
-		0,
-		D3D_DRIVER_TYPE_HARDWARE,
-		NULL,
-		0,
-		levels,
-		1,
-		D3D11_SDK_VERSION,
-		&device,
-		&supported,
-		&context
-	);
+	if (FAILED(initialise()))
+	{
+		return "rasteriser default culling test failed to initialise\n";
+	}
 
 	D3D11Renderer::Rasteriser unit;
 
@@ -54,4 +37,28 @@ std::string RasteriserDefaultCullingTest::test()
 	}
 
 	return "rasteriser default culling test failed\n";
+}
+
+HRESULT RasteriserDefaultCullingTest::initialise()
+{
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_11_0
+	};
+
+	D3D_FEATURE_LEVEL supported;
+
+	HRESULT output = D3D11CreateDevice(
+		0,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		0,
+		levels,
+		1,
+		D3D11_SDK_VERSION,
+		&device,
+		&supported,
+		&context
+	);
+
+	return output;
 }
--- TestSuite/src/application/rasteriser/default/RasteriserDefaultCullingTest.h
+++ TestSuite/src/application/rasteriser/default/RasteriserDefaultCullingTest.h
@@ -10,4 +10,12 @@ public:
 	~RasteriserDefaultCullingTest();
 
 	std::string test();
+
+private:
+	HRESULT initialise();
+
+private:
+	ID3D11Device* device;
+	ID3D11DeviceContext* context;
+	CD3D11_RASTERIZER_DESC description;
 };
