502ac584e873dca1a7c62d3085aeff97de836153
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-08 13:48:15 +0100

    RasteriserDefaultDepthTest initialise function defined

--- TestSuite/src/application/rasteriser/default/RasteriserDefaultDepthTest.cpp
+++ TestSuite/src/application/rasteriser/default/RasteriserDefaultDepthTest.cpp
@@ -11,24 +11,10 @@ RasteriserDefaultDepthTest::~RasteriserDefaultDepthTest()
 
 std::string RasteriserDefaultDepthTest::test()
 {
-	D3D_FEATURE_LEVEL levels[] = {
-		D3D_FEATURE_LEVEL_11_0
-	};
-
-	D3D_FEATURE_LEVEL supported;
-
-	HRESULT output = D3D11CreateDevice(
-		0,
-		D3D_DRIVER_TYPE_HARDWARE,
-		NULL,
-		0,
-		levels,
-		1,
-		D3D11_SDK_VERSION,
-		&device,
-		&supported,
-		&context
-	);
+	if (FAILED(initialise()))
+	{
+		return "rasteriser default depth test failed to initialise\n";
+	}
 
 	D3D11Renderer::Rasteriser unit;
 
@@ -51,4 +37,28 @@ std::string RasteriserDefaultDepthTest::test()
 	}
 
 	return "rasteriser default depth test failed\n";
+}
+
+HRESULT RasteriserDefaultDepthTest::initialise()
+{
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_11_0
+	};
+
+	D3D_FEATURE_LEVEL supported;
+
+	HRESULT output = D3D11CreateDevice(
+		0,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		0,
+		levels,
+		1,
+		D3D11_SDK_VERSION,
+		&device,
+		&supported,
+		&context
+	);
+
+	return output;
 }
