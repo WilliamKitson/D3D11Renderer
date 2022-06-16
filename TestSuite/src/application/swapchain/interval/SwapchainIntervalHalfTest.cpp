630e1eaa35a9f49803fcbbfa9d73627cafc98fe8
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-16 15:28:06 +0100

    SwapchainIntervalHalfTest initialiseD3D11 function defined

--- TestSuite/src/application/swapchain/interval/SwapchainIntervalHalfTest.cpp
+++ TestSuite/src/application/swapchain/interval/SwapchainIntervalHalfTest.cpp
@@ -3,6 +3,21 @@
 SwapchainIntervalHalfTest::SwapchainIntervalHalfTest(HINSTANCE hInstanceInput, int nCmdShowInput)
 	: hInstance{ hInstanceInput }, nCmdShow{ nCmdShowInput }, tag{ L"swapchain interval half test" }, window(), device{ nullptr }, context{ nullptr }, result(), unit()
 {
+	initialiseWindowClass();
+
+	if (FAILED(result))
+	{
+		return;
+	}
+
+	initialiseWindow();
+
+	if (FAILED(result))
+	{
+		return;
+	}
+
+	initialiseD3D11();
 }
 
 SwapchainIntervalHalfTest::~SwapchainIntervalHalfTest()
@@ -14,42 +29,9 @@ SwapchainIntervalHalfTest::~SwapchainIntervalHalfTest()
 
 std::string SwapchainIntervalHalfTest::test()
 {
-	initialiseWindowClass();
-
-	if (FAILED(result))
-	{
-		return "swapchain interval default test failed to initialise window class\n";
-	}
-
-	initialiseWindow();
-
 	if (FAILED(result))
 	{
 		return "swapchain interval default test failed to initialise window\n";
-	}
-
-	D3D_FEATURE_LEVEL levels[] = {
-		D3D_FEATURE_LEVEL_11_0
-	};
-
-	D3D_FEATURE_LEVEL supported;
-
-	result = D3D11CreateDevice(
-		0,
-		D3D_DRIVER_TYPE_HARDWARE,
-		NULL,
-		D3D11_CREATE_DEVICE_DEBUG,
-		levels,
-		1,
-		D3D11_SDK_VERSION,
-		&device,
-		&supported,
-		&context
-	);
-
-	if (FAILED(result))
-	{
-		return "swapchain interval default test failed to initialise window d3d11\n";
 	}
 
 	unit.initialise(device, window);
@@ -135,4 +117,26 @@ void SwapchainIntervalHalfTest::initialiseWindow()
 	);
 
 	result = S_OK;
+}
+
+void SwapchainIntervalHalfTest::initialiseD3D11()
+{
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_11_0
+	};
+
+	D3D_FEATURE_LEVEL supported;
+
+	result = D3D11CreateDevice(
+		0,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		D3D11_CREATE_DEVICE_DEBUG,
+		levels,
+		1,
+		D3D11_SDK_VERSION,
+		&device,
+		&supported,
+		&context
+	);
 }
