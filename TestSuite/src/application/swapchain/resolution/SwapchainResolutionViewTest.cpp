4e2a74cd3a73fce393fb429d558ad84a016ac1e3
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-06-16 10:13:57 +0100

    SwapchainResolutionViewTest initialiseD3D11 function defined

--- TestSuite/src/application/swapchain/resolution/SwapchainResolutionViewTest.cpp
+++ TestSuite/src/application/swapchain/resolution/SwapchainResolutionViewTest.cpp
@@ -3,6 +3,21 @@
 SwapchainResolutionViewTest::SwapchainResolutionViewTest(HINSTANCE hInstanceInput, int nCmdShowInput)
 	: hInstance{ hInstanceInput }, nCmdShow{ nCmdShowInput }, tag{ L"swapchain resolution view test" }, window(), device{ nullptr }, context{ nullptr }, result()
 {
+	initialiseWindowClass();
+
+	if (FAILED(result))
+	{
+		return;
+	}
+
+	initialiseWindow();
+
+	if (FAILED(result))
+	{
+		return;
+	}
+
+	initialiseD3D11();
 }
 
 SwapchainResolutionViewTest::~SwapchainResolutionViewTest()
@@ -14,42 +29,9 @@ SwapchainResolutionViewTest::~SwapchainResolutionViewTest()
 
 std::string SwapchainResolutionViewTest::test()
 {
-	initialiseWindowClass();
-
 	if (FAILED(result))
 	{
-		return "swapchain resolution view test failed to initialise window class\n";
-	}
-
-	initialiseWindow();
-
-	if (FAILED(result))
-	{
-		return "swapchain resolution view test failed to initialise window\n";
-	}
-
-	D3D_FEATURE_LEVEL levels[] = {
-		D3D_FEATURE_LEVEL_11_0
-	};
-
-	D3D_FEATURE_LEVEL supported;
-
-	result = D3D11CreateDevice(
-		0,
-		D3D_DRIVER_TYPE_HARDWARE,
-		NULL,
-		D3D11_CREATE_DEVICE_DEBUG,
-		levels,
-		1,
-		D3D11_SDK_VERSION,
-		&device,
-		&supported,
-		&context
-	);
-
-	if (FAILED(result))
-	{
-		return "swapchain resolution view test failed to initialise d3d11\n";
+		return "swapchain resolution view test failed to initialise\n";
 	}
 
 	int resolution[2]{
@@ -164,4 +146,26 @@ void SwapchainResolutionViewTest::initialiseWindow()
 
 		result = S_OK;
 	}
+}
+
+void SwapchainResolutionViewTest::initialiseD3D11()
+{
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_11_0
+	};
+
+	D3D_FEATURE_LEVEL supported;
+
+	result = D3D11CreateDevice(
+		0,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		D3D11_CREATE_DEVICE_DEBUG,
+		levels,
+		1,
+		D3D11_SDK_VERSION,
+		&device,
+		&supported,
+		&context
+	);
 }
