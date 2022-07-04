1f0a59b333db25313719d4afc69139b94b85da89
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-07-04 11:15:35 +0100

    RasteriserInvalidDeviceTest initialise function defined

--- TestSuite/src/application/rasteriser/invalid/RasteriserInvalidDeviceTest.cpp
+++ TestSuite/src/application/rasteriser/invalid/RasteriserInvalidDeviceTest.cpp
@@ -14,24 +14,12 @@ RasteriserInvalidDeviceTest::~RasteriserInvalidDeviceTest()
 
 std::string RasteriserInvalidDeviceTest::test()
 {
-	D3D_FEATURE_LEVEL levels[] = {
-		D3D_FEATURE_LEVEL_11_0
-	};
+	initialise();
 
-	D3D_FEATURE_LEVEL supported;
-
-	result = D3D11CreateDevice(
-		0,
-		D3D_DRIVER_TYPE_HARDWARE,
-		NULL,
-		D3D11_CREATE_DEVICE_DEBUG,
-		levels,
-		1,
-		D3D11_SDK_VERSION,
-		&device,
-		&supported,
-		&context
-	);
+	if (FAILED(result))
+	{
+		return "rasteriser invalid device test failed to initialise\n";
+	}
 
 	D3D11Renderer::Rasteriser unit;
 
@@ -54,4 +42,26 @@ void RasteriserInvalidDeviceTest::cleanup(IUnknown* input)
 	{
 		input->Release();
 	}
+}
+
+void RasteriserInvalidDeviceTest::initialise()
+{
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_11_0
+	};
+
+	D3D_FEATURE_LEVEL supported;
+
+	result = D3D11CreateDevice(
+		0,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		D3D11_CREATE_DEVICE_DEBUG,
+		levels,
+		1,
+		D3D11_SDK_VERSION,
+		&device,
+		&supported,
+		&context
+	);
 }
